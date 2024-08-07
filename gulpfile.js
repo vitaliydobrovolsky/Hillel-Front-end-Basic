cd034d1954d3429c2640711b11510400a58c978b
vitaliydobrovolsky <vitaliydobrovolsky986@gmail.com>
2024-08-07 19:48:03 +0300

    update 3

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+import gulpCssnano from "gulp-cssnano";
+import sass from "gulp-sass"
+
+/*gulp.task('sass', function() {
+    return gulp.src('app/css/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(Cssnano())
+        .pipe(gulp.dest('dist/css'))
+    
+});*/
